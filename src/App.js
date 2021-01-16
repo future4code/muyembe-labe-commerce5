4e36232d575a7401c126d0ecf1047c4805b263b3
ThiagodaSilva27 <thiagotwoseven@gmail.com>
2021-01-16 03:08:04 -0300

    iniciado componetização

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Produto from './componentes/Produto'
 import astronauta from './img/astronauta.png'
 import camisa1 from './img/camisa1.png'
 import camisa2 from './img/camisa2.png'
@@ -10,12 +11,20 @@ import camisa6 from './img/camisa6.png'
 import camisa7 from './img/camisa7.png'
 import camisa8 from './img/camisa8.png'
 import camisa9 from './img/camisa9.png'
+import camisaF1 from './img/camisaF1.png'
+import camisaF2 from './img/camisaF2.png'
+import camisaF3 from './img/camisaF3.png'
+import camisaF4 from './img/camisaF4.png'
+import camisaF5 from './img/camisaF5.png'
+import camisaF6 from './img/camisaF6.png'
 import triangulo from './img/triangulo.PNG'
+import subir from './img/subir.PNG'
 
 const Fundo = styled.div`
   margin: 0;
   padding: 0;
   background-color: #0e0d09;
+  position:relative;
 `
 
 const Titulo = styled.header`
@@ -39,11 +48,22 @@ const Lista = styled.li`
   margin-bottom: 10px;
   text-align: center;
   padding: 5px;
+  &:hover{
+    box-shadow: 2px 5px 10px #D0A9F5;
+  }
 `
 
 const Imghome = styled.img`
   margin-left: 20%;
 `
+
+const Subir = styled.img`
+  bottom:0px;
+  right:3px;
+  width:100px;
+  position:absolute;
+`
+
 const Referencia = styled.a`
   text-decoration:none; 
   color: inherit; 
@@ -54,26 +74,8 @@ const ListaDeProdutos = styled.div`
   flex-wrap:wrap;
   flex-direction:row;
 `
-const Preco = styled.p`
-  display:inline;
-  color: white;
-  font-size:40px;
-  padding:30px;
-  margin-left:38px;
-`
-const Add = styled.button`
-  display: inline;
-  font-size:20px;
-  background-color: transparent;
-  color:green;
-  border-style: none;
-  border: solid green 1px;
-`
-const Produto = styled.div`
-  margin: 10px 5px 10px 15px;
-  width:400px;
-  background-color: #191512;
-`
+
+
 const Tituloprodutos = styled.h1`
   color: #A901DB;
   font-family: 'Courier New', Courier, monospace;
@@ -100,86 +102,41 @@ const Rodape = styled.footer`
 function App() {
   return (
     <Fundo>
-      <Titulo>Labecommerce</Titulo>
+      <Titulo id="voltarAotopo">Labecommerce</Titulo>
       <Banner>
         <Triangulo src={triangulo}/>
         <Menu>
           <ul>
             <Lista>Home</Lista>
+            <Lista><Referencia href="#feminino">Feminino</Referencia></Lista>
             <Lista><Referencia href="#masculino" >Masculino</Referencia></Lista>
-            <Lista>Feminino</Lista>
-            <Lista>Infantil</Lista>
           </ul>
         </Menu>
         <div><Imghome src={astronauta}/></div>
       </Banner>
 
-      <Tituloprodutos id="masculino">Produtos</Tituloprodutos>
+      <Tituloprodutos id="feminino">Produtos</Tituloprodutos>
       <ListaDeProdutos>
         
-        <Produto>
-          <div>
-            <img src={camisa1}/>
-            <Preco>R$ 59,90</Preco><Add>+</Add>
-          </div>
-        </Produto>
+        <Produto
+          tituloProduto={"camisa1"}
+          produtoCamisa={camisa1}
+          precoProduto={"R$ 59,90"}
+        />
 
-        <Produto>
-          <div>
-            <img src={camisa2}/>
-            <Preco>R$ 59,90</Preco><Add>+</Add>
-          </div>
-        </Produto>
+        <Produto
+          tituloProduto={"camisa2"}
+          produtoCamisa={camisa2}
+          precoProduto={"R$ 59,90"}
+        />
 
-        <Produto>
-          <div>
-            <img src={camisa3}/>
-            <Preco>R$ 59,90</Preco><Add>+</Add>
-          </div>
-        </Produto>
-
-        <Produto>
-          <div>
-            <img src={camisa4}/>
-            <Preco>R$ 59,90</Preco><Add>+</Add>
-          </div>
-        </Produto>
-
-        <Produto>
-          <div>
-            <img src={camisa5}/>
-            <Preco>R$ 59,90</Preco><Add>+</Add>
-          </div>
-        </Produto>
-
-        <Produto>
-          <div>
-            <img src={camisa6}/>
-            <Preco>R$ 59,90</Preco><Add>+</Add>
-          </div>
-        </Produto>
-
-        <Produto>
-          <div>
-            <img src={camisa7}/>
-            <Preco>R$ 59,90</Preco><Add>+</Add>
-          </div>
-        </Produto>
-
-        <Produto>
-          <div>
-            <img src={camisa8}/>
-            <Preco>R$ 59,90</Preco><Add>+</Add>
-          </div>
-        </Produto>
-
-        <Produto>
-          <div>
-            <img src={camisa9}/>
-            <Preco>R$ 59,90</Preco><Add>+</Add>
-          </div>
-        </Produto>
+        <Produto 
+          tituloProduto={"camisa3"}
+          produtoCamisa={camisa9}
+          precoProduto={"R$ 59,90"}
+        />
       </ListaDeProdutos>
+      <a href="#voltarAotopo"><Subir src={subir}/></a>
       <Rodape>
         <h3>Labecommerce faz parte da coorporação Astrodev</h3>
         <p></p>
---
+++ src/componentes/Produto.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Produtos = styled.div`
+  margin: 10px 5px 10px 15px;
+  width:400px;
+  background-color: #191512;
+`
+const Imgproduto = styled.img`
+  width:300px;
+`
+const Preco = styled.p`
+  display:inline;
+  color: white;
+  font-size:40px;
+  padding:30px;
+  margin-left:38px;
+`
+const Add = styled.button`
+  display: inline;
+  font-size:20px;
+  background-color: transparent;
+  color:green;
+  border-style: none;
+  border: solid green 1px;
+  &:hover{
+    background-color:#81F7BE;
+  }
+`
+const NomeProduto = styled.h3`
+    color:white;
+    padding-left:25px;
+`
+
+class Produto extends React.Component { 
+    render() {
+        return(
+        <Produtos>
+            <div>
+            <Imgproduto src={this.props.produtoCamisa} alt="imagem do produto"/>
+            <NomeProduto>{this.props.tituloProduto}</NomeProduto>
+            <Preco>{this.props.precoProduto}</Preco><Add>+</Add>
+            </div>
+        </Produtos>
+        )
+    }
+}
+
+export default Produto;
